cdcda6c4bfefb6f5c82f2871b774451e2acbeffd
Joris te Dorsthorst <6518350+Joristdh@users.noreply.github.com>
2024-03-26 17:27:15 +0100

    solid base

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,6 @@ import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
 
 export default defineConfig({
-  plugins: [solidPlugin(),],
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: 'esnext',
-  },
+  base: '/solid',
+  plugins: [solidPlugin()],
 })
