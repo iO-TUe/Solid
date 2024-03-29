be864e3ce465ae8f573bbb7ddd90330f8c584392
Joris te Dorsthorst <6518350+Joristdh@users.noreply.github.com>
2024-03-29 15:21:19 +0100

    same class stroke

--- src/components/Gauge.tsx
+++ src/components/Gauge.tsx
@@ -4,12 +4,11 @@ import styles from './gauge.module.css'
 const Gauge: Component<{ value: number, recurse: boolean }> = (props) => {
   const [bool, setBool] = createSignal(false)
 
-  console.log("Script: Gauge")
-
   setTimeout(() => setBool(props.recurse), 0)
 
+  console.log("Script: Gauge", `[${props.value}]`)
   return <>
-    {console.log("Render: Gauge")}
+    {console.log("Render: Gauge", `[${props.value}]`)}
     <div role='feed' class={styles.wrapper} onclick={() => setBool(true)} >
       <svg viewBox="0 0 120 120" class={styles.gauge}>
         <defs>
@@ -21,18 +20,13 @@ const Gauge: Component<{ value: number, recurse: boolean }> = (props) => {
 
         <circle r="56" cx="60" cy="60" stroke-width="8" style="fill: #000; stroke: #0000"></circle>
 
-        <circle
-          r="56"
-          cx="60"
-          cy="60"
-          stroke-width="8"
-          style={`transform: rotate(-87.9537deg); stroke-dasharray: ${props.value * 3.51
-            }, 351.858; fill:none; transform-origin:50% 50%; stroke-linecap:round; stroke:url(#gradient)`}
+        <circle r="56" cx="60" cy="60" stroke-width="8" class={styles.stroke}
+          style={`stroke-dasharray: ${props.value * 3.51}, 351.858;`}
         ></circle>
       </svg>
       <span class={styles.value}>{props.value}</span>
     </div>
-    {(bool() && props.value < 120) && <div class="recurse"><Gauge value={props.value + 1} recurse={true} /><Gauge value={props.value + 5} recurse={true} /></div>}
+    {(bool() && props.value < 100) && <div class="recurse"><Gauge value={props.value + 1} recurse={true} /><Gauge value={props.value + 5} recurse={true} /></div>}
   </>
 }
 
