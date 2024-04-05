bb7960a3b47302bb3be1f525e4d2673c90f210ef
Joris te Dorsthorst <6518350+Joristdh@users.noreply.github.com>
2024-04-05 14:38:18 +0200

    Yes comps

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
 /* @refresh reload */
 import { Route, Router } from '@solidjs/router'
 import { render } from 'solid-js/web'
-// import Counter from './components/counter'
-// import Header from './components/header'
+import Counter from './components/counter'
+import Header from './components/header'
 import './index.css'
 
 render(() =>
   <div class="App">
-    {/* <Header /> */}
+    <Header />
     <main class="App-main">
       <Router>
-        {/* <Route path='/load' component={() => <Counter initialValue={50} maxValue={1000} recurse={false} />} />
-        <Route path='/' component={() => <Counter initialValue={80} maxValue={13} recurse={true} />} /> */}
+        <Route path='/load' component={() => <Counter initialValue={50} maxValue={1000} recurse={false} />} />
+        <Route path='/' component={() => <Counter initialValue={80} maxValue={13} recurse={true} />} />
       </Router>
     </main>
   </div>
